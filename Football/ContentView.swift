70da8287efa4887366b8a283016296d087c753b2
Ignacio <ignaciocantoisado@gmail.com>
2025-08-28 15:30:32 -0400

    Initial Commit

---
+++ Football/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football
+//
+//  Created by Ignacio Canto on 8/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Ignacio Canto on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
